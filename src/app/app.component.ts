c8e1dcda9e311c2953991371985089c6ddcea886
balajigajen8 <93248230+balajigajen8@users.noreply.github.com>
2024-06-28 18:09:18 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+[x: string]: any;
+store: any;
+name = 'BALAJI';
+titleParts = [
+  { text: 'MADE', colorClass: 'indian-product-saffron' },
+  { text: 'IN', colorClass: 'indian-product-white' },
+  { text: 'INDIA', colorClass: 'indian-product-green' }
+];
+}
